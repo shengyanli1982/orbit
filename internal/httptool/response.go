054b3842e1b70f180d557a6e59d46c9bada9676e
shengyanli1982 <shengyanlee36@gmail.com>
2024-01-13 20:39:17 +0800

    Complete the missing code remarks

--- internal/httptool/response.go
+++ internal/httptool/response.go
@@ -7,10 +7,13 @@ import (
 )
 
 type ResponseBodyWriter struct {
+	// gin.ResponseWriter is an interface that encapsulates the http.ResponseWriter interface.
 	gin.ResponseWriter
+	// buffer is a bytes.Buffer instance.
 	buffer *bytes.Buffer
 }
 
+// NewResponseBodyWriter returns a new ResponseBodyWriter instance.
 func NewResponseBodyWriter(w gin.ResponseWriter, buf *bytes.Buffer) *ResponseBodyWriter {
 	return &ResponseBodyWriter{
 		ResponseWriter: w,
@@ -18,6 +21,7 @@ func NewResponseBodyWriter(w gin.ResponseWriter, buf *bytes.Buffer) *ResponseBod
 	}
 }
 
+// Write writes the data to the connection.
 func (w *ResponseBodyWriter) Write(b []byte) (int, error) {
 	if count, err := w.buffer.Write(b); err != nil {
 		return count, err
@@ -25,6 +29,7 @@ func (w *ResponseBodyWriter) Write(b []byte) (int, error) {
 	return w.ResponseWriter.Write(b)
 }
 
+// WriteString writes the contents of the string s to w, which accepts a slice of bytes.
 func (w *ResponseBodyWriter) WriteString(s string) (int, error) {
 	if count, err := w.buffer.WriteString(s); err != nil {
 		return count, err
@@ -32,14 +37,17 @@ func (w *ResponseBodyWriter) WriteString(s string) (int, error) {
 	return w.ResponseWriter.WriteString(s)
 }
 
+// Reset resets the buffer.
 func (w *ResponseBodyWriter) Reset() {
 	w.buffer = nil
 }
 
+// GetBuffer returns the buffer.
 func (w *ResponseBodyWriter) GetBuffer() *bytes.Buffer {
 	return w.buffer
 }
 
+// GetResponseWriter returns the ResponseWriter.
 func (w *ResponseBodyWriter) GetResponseWriter() gin.ResponseWriter {
 	return w.ResponseWriter
 }
